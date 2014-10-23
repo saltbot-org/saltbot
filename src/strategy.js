809fa20aaaffcfc7b115dc46bccb781def41eabc
David <kyran36@hotmail.com>
2014-10-23 13:16:18 +0900

    improved debug messages

--- src/strategy.js
+++ src/strategy.js
@@ -221,70 +221,7 @@ ConfidenceScore.prototype.getWeightedScores = function(c, hasTiered, hasUntiered
 	}
 };
 ConfidenceScore.prototype.adjustConfidence = function() {
-	var numberOfFactors;
-	var multipliers;
-	var oddsConfidence;
-	var confidence = this.confidence;
-	// the below line was the original way it was set up
-	// this.confidence = this.fallback1.confidence || 0.1;
-	var fallbackConfidence = this.fallback1.confidence || 0.1;
-	var oc = this.oddsConfidence;
-	confidence *= this.chromosome.confidenceWeight;
-	fallbackConfidence *= this.chromosome.fallbackConfidenceWeight;
 
-	if (oc) {
-		numberOfFactors = 3;
-		multipliers = (this.chromosome.confidenceWeight + this.chromosome.fallbackConfidenceWeight + this.chromosome.oddsConfidenceWeight) / numberOfFactors;
-		oddsConfidence = (oc[0] > oc[1]) ? oc[0] / (oc[0] + oc[1]) : oc[1] / (oc[0] + oc[1]);
-		oddsConfidence *= this.chromosome.oddsConfidenceWeight;
-		this.confidence = (confidence + fallbackConfidence + oddsConfidence) / numberOfFactors / multipliers;
-	} else {
-		numberOfFactors = 2;
-		multipliers = (this.chromosome.confidenceWeight + this.chromosome.fallbackConfidenceWeight ) / numberOfFactors;
-		this.confidence = (confidence + fallbackConfidence ) / numberOfFactors / multipliers;
-	}
-
-	var unconfident = false;
-	var minCon = this.chromosome.minimumCombinedConfidenceForLargeBet;
-	if (minCon > 1)
-		minCon = 1 / minCon;
-	if (this.confidence < minCon) {
-		unconfident = true;
-		if (this.debug)
-			console.log("- combined confidence too low, dropping confidence by 75%");
-	} else if (this.lowerTotalMatches < Math.ceil(this.chromosome.minimumMatchesForLargeBet)) {
-		unconfident = true;
-		if (this.debug)
-			console.log("- one or both players have too few matches, dropping confidence by 75%");
-	} else if (this.c1WWinPercentage < .25 && this.c2WWinPercentage < .25) {
-		// that .25 could possibly be given to the chromosome
-		unconfident = true;
-		if (this.debug)
-			console.log("- both players are losers, dropping confidence by 75%");
-	} else if (this.lowBet) {
-		unconfident = true;
-		if (this.debug)
-			console.log("- insufficient information, dropping confidence by 75% -> " + this.insufficientInformationMessage);
-	} else {
-		var oP = null;
-		var fbP = this.fallback1.prediction;
-		if (oc)
-			oP = (oc[0] > oc[1]) ? this.c1name : this.c2name;
-		if ((oP != null && oP != this.prediction) || (fbP != null && fbP != this.prediction) || (oP != null && fbP != null && oP != fbP)) {
-			unconfident = true;
-			if (this.debug)
-				console.log("- the jury disagrees, dropping confidence by 75%");
-		}
-	}
-	//See if the Jury disagrees
-
-	if (unconfident)
-		this.confidence *= .25;
-
-	// subtract confidence from 50%, 50% is when you know nothing
-	this.confidence -= .5;
-	if (this.confidence < 0)
-		this.confidence = .01;
 };
 ConfidenceScore.prototype.execute = function(info) {
 	var c1 = info.character1;
@@ -362,15 +299,15 @@ ConfidenceScore.prototype.execute = function(info) {
 	var c2WPotential = c2WLScores[0] + c2WLScores[1];
 
 	// used for jury calculation also, hence "this"
-	this.c1WWinPercentage = 0;
-	this.c2WWinPercentage = 0;
+	var c1WWinPercentage = 0;
+	var c2WWinPercentage = 0;
 	if (c1WPotential != 0 && c2WPotential != 0) {
-		this.c1WWinPercentage = c1WLScores[0] / c1WPotential;
-		this.c2WWinPercentage = c2WLScores[0] / c2WPotential;
+		c1WWinPercentage = c1WLScores[0] / c1WPotential;
+		c2WWinPercentage = c2WLScores[0] / c2WPotential;
 
-		if (this.c1WWinPercentage > this.c2WWinPercentage)
+		if (c1WWinPercentage > c2WWinPercentage)
 			c1Score += winPercentageWeight;
-		else if (this.c2WWinPercentage > this.c1WWinPercentage)
+		else if (c2WWinPercentage > c1WWinPercentage)
 			c2Score += winPercentageWeight;
 	}
 
@@ -409,16 +346,78 @@ ConfidenceScore.prototype.execute = function(info) {
 	// the point scoring in this makes for a terrible confidence predictor; rely on this for betting amount instead
 	this.fallback1.execute(info);
 
-	if ((c1Score == c2Score) || (c1.wins.length == 0 && c1.losses.length == 0) || (c2.wins.length == 0 && c2.losses.length == 0)) {
-		// if (this.debug)
-		// console.log("- CS has insufficient information ");
-		this.abstain = true;
-		this.lowBet = true;
-		this.insufficientInformationMessage = "(scores: " + c1Score.toFixed(2) + ":" + c2Score.toFixed(2) + "), W:L(P1)(P2)-> (" + c1.wins.length + ":" + c1.losses.length + ")(" + c2.wins.length + ":" + c2.losses.length + ")";
-		return this.prediction;
-	}
 	if (this.debug)
 		console.log("- " + this.prediction + " has a better W score (scores: " + c1Score.toFixed(2) + ":" + c2Score.toFixed(2) + "), W:L(P1)(P2)-> (" + c1.wins.length + ":" + c1.losses.length + ")(" + c2.wins.length + ":" + c2.losses.length + "), betting " + this.prediction);
+
+	/*---------------------------------------------------------------------------------------------------*/
+	// CONFIDENCE ADJUSTMENT SECTION
+	/*---------------------------------------------------------------------------------------------------*/
+
+	var numberOfFactors;
+	var multipliers;
+	var oddsConfidence;
+	var confidence = this.confidence;
+	var fallbackConfidence = this.fallback1.confidence || 0.1;
+	var oc = this.oddsConfidence;
+	confidence *= this.chromosome.confidenceWeight;
+	fallbackConfidence *= this.chromosome.fallbackConfidenceWeight;
+
+	if (oc) {
+		numberOfFactors = 3;
+		multipliers = (this.chromosome.confidenceWeight + this.chromosome.fallbackConfidenceWeight + this.chromosome.oddsConfidenceWeight) / numberOfFactors;
+		oddsConfidence = (oc[0] > oc[1]) ? oc[0] / (oc[0] + oc[1]) : oc[1] / (oc[0] + oc[1]);
+		oddsConfidence *= this.chromosome.oddsConfidenceWeight;
+		this.confidence = (confidence + fallbackConfidence + oddsConfidence) / multipliers / numberOfFactors;
+	} else {
+		numberOfFactors = 2;
+		multipliers = (this.chromosome.confidenceWeight + this.chromosome.fallbackConfidenceWeight ) / numberOfFactors;
+		this.confidence = (confidence + fallbackConfidence ) / multipliers / numberOfFactors;
+	}
+
+	var unconfident = false;
+	var minCon = this.chromosome.minimumCombinedConfidenceForLargeBet;
+	if (minCon > 1)
+		minCon = 1 / minCon;
+	if (this.confidence < minCon) {
+		unconfident = true;
+		if (this.debug)
+			console.log("- combined confidence too low (cf:" + this.confidence.toFixed(4) + ", rq:" + minCon.toFixed(4) + "), dropping confidence by 75%");
+	} else if (this.lowerTotalMatches < Math.ceil(this.chromosome.minimumMatchesForLargeBet)) {
+		unconfident = true;
+		if (this.debug)
+			console.log("- one or both players have too few matches (" + c1.wins.length + ":" + c1.losses.length + ")(" + c2.wins.length + ":" + c2.losses.length + "), dropping confidence by 75%");
+	} else if (c1WWinPercentage < .25 && c2WWinPercentage < .25) {
+		// that .25 could possibly be given to the chromosome
+		unconfident = true;
+		if (this.debug)
+			console.log("- both players are losers (<25% weighted win rate) c1: " + (c1WWinPercentage * 100).toFixed(2) + "%, c2: " + (c2WWinPercentage * 100).toFixed(2) + "%, dropping confidence by 75%");
+	} else if ((c1Score == c2Score) || (c1.wins.length == 0 && c1.losses.length == 0) || (c2.wins.length == 0 && c2.losses.length == 0)) {
+		unconfident = true;
+		if (this.debug)
+			console.log("- insufficient information (scores: " + c1Score.toFixed(2) + ":" + c2Score.toFixed(2) + "), W:L(P1)(P2)-> (" + c1.wins.length + ":" + c1.losses.length + ")(" + c2.wins.length + ":" + c2.losses.length + "), dropping confidence by 75% -> " + this.insufficientInformationMessage);
+	} else {
+		var oP = null;
+		var fbP = this.fallback1.prediction;
+		if (oc)
+			oP = (oc[0] > oc[1]) ? this.c1name : this.c2name;
+		if ((oP != null && oP != this.prediction) || (fbP != null && fbP != this.prediction) || (oP != null && fbP != null && oP != fbP)) {
+			unconfident = true;
+			if (this.debug)
+				console.log("- the jury disagrees, dropping confidence by 75%");
+		}
+	}
+
+	if (unconfident) {
+		this.abstain = true;
+		this.lowBet = true;
+		this.confidence *= .25;
+	}
+
+	// subtract confidence from 50%, 50% is when you know nothing
+	this.confidence -= .5;
+	if (this.confidence < 0)
+		this.confidence = .01;
+
 	return this.prediction;
 };
 
