e843c822170eba59d3696c094401a204299dbc59
reconman <madara.1212@yahoo.de>
2015-07-14 02:10:04 +0200

    Added a tournament indicator for the final round.

--- src/tracker.js
+++ src/tracker.js
@@ -120,8 +120,10 @@ Match.prototype.init = function() {
 			setTimeout(function() {
 				var tournamentModeIndicator = "characters are left in the bracket!";
 				var tournamentModeIndicator2 = "Tournament mode start";
+				var tournamentModeIndicator3 = "FINAL ROUND! Stay tuned for exhibitions after the tournament!"
 				var footer = document.getElementById("footer-alert");
-				var tournament = footer != null && (footer.innerHTML.indexOf(tournamentModeIndicator) > -1 || footer.innerHTML.indexOf(tournamentModeIndicator2) > -1);
+				var tournament = footer != null && (footer.innerHTML.indexOf(tournamentModeIndicator) > -1 || footer.innerHTML.indexOf(tournamentModeIndicator2) > -1 ||
+													footer.innerHTML.indexOf(tournamentModeIndicator3) > -1);
 
 				self.betAmount(tournament);
 
