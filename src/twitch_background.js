f1ec391cf8f896aa34630197236624c7cc058993
Stefan Berger <madara.1212@yahoo.de>
2015-07-24 21:34:57 +0200

    Corrected the filter URL so it also accepts live. or mugen.saltybet.com

--- src/twitch_background.js
+++ src/twitch_background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function() {
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
                     pageUrl: {
-                        hostEquals: 'www.saltybet.com'
+                        hostEquals: 'saltybet.com'
                     }
                 })
             ],
