e5eefa3451efb3582e1ffd61ae792b976454b668
aciil <ACIIL@users.noreply.github.com>
2017-03-12 09:15:34 -0600

    tweaks

--- src/strategy.js
+++ src/strategy.js
@@ -284,8 +284,8 @@ Chromosome.prototype.toDisplayString = function () {
 Chromosome.prototype.mate = function (other) {
 	var offspring = new Chromosome();
 	for (var i in offspring) {
-		var mutationScale = 0.20;	// range 0..<1 (a danger if offspring weight becomes < 0).
-		var mutationChance = 0.15;	// range 0..1
+		var mutationScale = 0.35;	// range 0..<1 (a danger if offspring weight becomes < 0).
+		var mutationChance = 0.08;	// range 0..1
 		if (typeof offspring[i] != "function") {
 			offspring[i] = (Math.random() > 0.5) ? this[i] : other[i];
 			var radiation =  (Math.random() - 0.5) * 2.0;
