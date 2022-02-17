43ae5b6fdf55931b574533d374086bcf14c968dc
reconman <reconman@users.noreply.github.com>
2022-02-18 00:11:51 +0100

    Change spaces to tabs

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,6 @@
 import type { Controller } from "./salty";
 
 export class Globals {
-    public static ctrl: Controller = null;
-    public static dirtyRecords = true;
+	public static ctrl: Controller = null;
+	public static dirtyRecords = true;
 }
