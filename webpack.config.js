e9ad28836886560ad099b15981419f218fd78ee5
reconman <reconman@users.noreply.github.com>
2022-04-29 18:11:30 +0200

    Copy moment.min.js from node_modules for auto-updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -10,7 +11,7 @@ module.exports = {
         popup: join(__dirname, 'src/popup.ts'),
         config: join(__dirname, 'src/config.ts'),
         matchdata: join(__dirname, 'src/matchdata.ts'),
-		chat: join(__dirname, 'src/twitch_content.ts')
+        chat: join(__dirname, 'src/twitch_content.ts')
     },
     output: {
         path: join(__dirname, 'dist', 'js'),
@@ -27,11 +28,18 @@ module.exports = {
             }
         ],
     },
-    plugins: [new ESLintPlugin(
+    plugins: [
+      new ESLintPlugin(
         {
             extensions: [".ts"],
         }
-    )],
+      ),
+      new CopyPlugin({
+        patterns: [
+          { from: "node_modules/moment/min/moment.min.js", to: "lib/moment.min.js" }
+        ],
+      }),
+    ],
     resolve: {
         extensions: ['.ts', '.js'],
     },
